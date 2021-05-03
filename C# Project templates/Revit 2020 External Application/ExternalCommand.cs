d16a32c5a31c6cb838b35fe028038bda1425e3c7
Alexander Shchetinin <a.shchetinin@gmail.com>
2021-05-03 11:58:27 +0500

    comment RevitDevTools

--- C# Project templates/Revit 2020 External Application/ExternalCommand.cs
+++ C# Project templates/Revit 2020 External Application/ExternalCommand.cs
@@ -25,7 +25,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Interop;
 using WPF = System.Windows;
 using System.Linq;
-using Bushman.RevitDevTools;
+//using Bushman.RevitDevTools;
 using $RootNamespace$.$safeprojectname$.Properties;
 #endregion
 
